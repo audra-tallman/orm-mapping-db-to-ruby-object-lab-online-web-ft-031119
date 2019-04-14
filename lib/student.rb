dfa2238029e0d92a00f039a3487e51a4eb77cfde
Audra Tallman <audi.tallman@gmail.com>
2019-04-14 15:34:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -125,7 +125,17 @@ class Student
     end.first
   end 
   
-  def self.all_students_in_grade_x
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      ORDER BY students.id
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
   end 
   
   
