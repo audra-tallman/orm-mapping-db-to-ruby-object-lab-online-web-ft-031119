06664678fdc89ccc68f7859e543a7dce918e4ab5
Audra Tallman <audi.tallman@gmail.com>
2019-04-14 15:23:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,10 +67,9 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 9
-      LIMIT 1
     SQL
  
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end 
   end 
